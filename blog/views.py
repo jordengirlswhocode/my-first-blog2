8868f41b1c2636cefb13df98ed40b1f3027139de
jordengirlswhocode <jorden.jolley@girlswhocode.com>
2017-08-02 10:04:54 -0700

    modified html

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post #importing models data so we can make it dynamic
+
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
     #takes in their request and renders an html page
