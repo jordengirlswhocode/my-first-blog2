a177ad59f493c50daf20dd1e80ab3988a2eb4960
jordengirlswhocode <jorden.jolley@girlswhocode.com>
2017-08-02 09:40:40 -0700

    Changed HTML

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+    #takes in their request and renders an html page
